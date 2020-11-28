6945884557db124bb41cbde117d82ad8b55ce1ab
atanassov-daniel <59175862+atanassov-daniel@users.noreply.github.com>
2020-11-28 17:25:32 +0200

    Finished with the Books project

--- 4RemoteDatabases/Exercise/BOOKS/app.js
+++ 4RemoteDatabases/Exercise/BOOKS/app.js
@@ -1,3 +1,92 @@
+// ако натисна на някой EDIT без да съм submit-нал предния натиснат няма да се получи
+// const {
+//     editBook,
+//     deleteHandler
+// } = (function () {
+//     return {
+async function editBook(e) {
+    const bookToChange = e.target.parentElement.parentElement;
+    const bookID = bookToChange.id;
+
+    const oldTitle = bookToChange.querySelector(".title").textContent;
+    const oldAuthor = bookToChange.querySelector(".author").textContent;
+    const oldIsbn = bookToChange.querySelector(".isbn").textContent;
+
+    const form = document.createElement("form");
+    form.id = "edit-form";
+    form.style.display = "none";
+    form.innerHTML = `<h3>FORM</h3>
+            <label>TITLE</label>
+            <input type="title" id="new-title" placeholder="New title..." value="${oldTitle}">
+            <label>AUTHOR</label>
+            <input type="title" id="new-author" placeholder="New author..." value="${oldAuthor}">
+            <label>ISBN</label>
+            <input type="title" id="new-isbn" placeholder="New isbn..." value="${oldIsbn}">
+            <button id="edit-form-submit-button">Submit change</button>`;
+    if (document.querySelectorAll("form#edit-form").length >= 1) return;
+    // if (document.querySelectorAll("form#edit-form")) return;
+    document.querySelector("body").appendChild(form);
+
+    document.getElementById("new-book-form").style.display = "none"; // make the form for editing the book visible
+    form.style.display = "block"; // make the form for editing the book visible
+
+    // alert("Please fill in the form down below");
+    document.getElementById("edit-form-submit-button").scrollIntoView({
+        behavior: "smooth"
+    });
+
+    document.getElementById("edit-form-submit-button").addEventListener("click", async function submitChange(e) {
+        e.preventDefault();
+
+        const [title, author, isbn] = [document.getElementById("new-title"), document.getElementById("new-author"), document.getElementById("new-isbn")];
+        const [titleValue, authorValue, isbnValue] = [title.value, author.value, isbn.value];
+
+        const newBook = {
+            "title": titleValue.trim(),
+            "author": authorValue.trim(),
+            "isbn": isbnValue.trim()
+        };
+        if (titleValue.trim() === "" || authorValue.trim() === "" || isbnValue.trim() === "") {
+            alert("Please fill in all fields of the form");
+            return;
+        }
+
+        const response = await fetch(`${baseUrl}/${bookID}.json`, { // ${}
+            method: "PATCH",
+            body: JSON.stringify(newBook)
+        });
+        if (response.ok === false) {
+            console.log(`${response.status} (${response.statusText}) - The entry couldn't be updated on the server`);
+            return;
+        }
+
+        const bookObj = await response.json();
+        // console.log(bookObj);
+        document.getElementById("edit-form").remove();
+        document.getElementById("new-book-form").style.display = "block";
+
+        const row = [...document.querySelectorAll("tr")].filter(book => book.id === bookID)[0];
+        row.querySelector("td.title").textContent = newBook.title;
+        row.querySelector("td.author").textContent = newBook.author;
+        row.querySelector("td.isbn").textContent = newBook.isbn;
+
+        document.getElementById(bookID).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+} //,
+// 
+async function deleteHandler(e) {
+    const row = e.target.parentElement.parentElement;
+
+    const response = await fetch(`${baseUrl}/${row.id}.json`, {
+        method: "DELETE"
+    });
+    response.ok === true ? row.remove() : console.log(`${response.status} (${response.statusText}) - The entry couldn't be deleted from the server`);
+}
+// };
+// })();
+
 // онези "AT THE SERVER SIDE" за DELETE и EDIT в условията значат ли, че трябва да трия само от сървъра, а при натискане на бутона LOAD ALL BOOKS само да трябва да ги упдейтвам, в това май няма много логика
 const baseUrl = `https://books-db1.firebaseio.com/books`;
 const tableBooks = document.querySelectorAll("table")[0];
@@ -16,40 +105,39 @@ async function loadAllBooks(e) {
 
         const bookRow = document.createElement("tr");
         bookRow.id = key;
-        const htmlBookRow = `<td>${bookObj.title}</td><td>${bookObj.author}</td><td>${bookObj.isbn}</td>`;
+        const htmlBookRow = `<td class="title">${bookObj.title}</td><td class="author">${bookObj.author}</td><td class="isbn">${bookObj.isbn}</td>`;
         bookRow.innerHTML = htmlBookRow;
         // <td><button>Edit</button><button>Delete</button></td>
-        const td = document.createElement("td");
+        const tdActions = document.createElement("td");
+        tdActions.className = "actions";
         const editButton = document.createElement("button");
         editButton.textContent = "Edit";
-        td.appendChild(editButton);
+        tdActions.appendChild(editButton);
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
-        td.appendChild(deleteButton);
-        deleteButton.addEventListener("click", async function deleteHandler(e) {
-            const row = e.target.parentElement.parentElement;
+        tdActions.appendChild(deleteButton);
 
-            await fetch(`${baseUrl}/${row.id}.json`, {
-                    method: "DELETE"
-                })
-                .then(resp => resp.ok === true ? row.remove() : console.log(`${resp.status} (${resp.statusText}) - The entry couldn't be deleted from the server`));
-        });
+        editButton.addEventListener("click", editBook);
+        deleteButton.addEventListener("click", deleteHandler);
 
-        bookRow.appendChild(td);
+        bookRow.appendChild(tdActions);
 
         tableBody.appendChild(bookRow);
     });
 }
+
 loadAllBooks(); // load all books when the page loads
 document.getElementById("loadBooks").addEventListener("click", loadAllBooks); // load the books when the button gets clicked
 
+// set an id to the form for a new item
+document.querySelector("form").id = "new-book-form";
 // get the 'Submit' button of the form
 const formSubmitButton = [...document.querySelectorAll(`form button`)].filter(button => button.innerText = "Submit")[0];
 formSubmitButton.id = "form-submit-button";
 
 const [title, author, isbn] = [document.getElementById("title"), document.getElementById("author"), document.getElementById("isbn")];
 
-formSubmitButton.addEventListener("click", async function (e) {
+formSubmitButton.addEventListener("click", async function postNewBook(e) {
     // maybe I should try and check if there is already an entry with the same title, author and isbn
     e.preventDefault();
 
@@ -59,31 +147,18 @@ formSubmitButton.addEventListener("click", async function (e) {
         "author": authorValue.trim(),
         "isbn": isbnValue.trim()
     };
-    if (titleValue.trim() === "" || authorValue.trim() === "" || isbnValue.trim() === "") return;
+    if (titleValue.trim() === "" || authorValue.trim() === "" || isbnValue.trim() === "") {
+        alert("Please fill in all fields of the form");
+        return;
+    }
 
-    fetch(`${baseUrl}.json`, {
-            method: "POST",
-            body: JSON.stringify(newBook)
-        })
-        .then(resp => resp.ok === true ? loadAllBooks() : console.log(`${resp.status} (${resp.statusText}) - The entry couldn't be posted on the server`));
+    const response = await fetch(`${baseUrl}.json`, {
+        method: "POST",
+        body: JSON.stringify(newBook)
+    });
+    response.ok === true ? loadAllBooks() : console.log(`${response.status} (${response.statusText}) - The entry couldn't be posted on the server`);
 
     title.value = "";
     author.value = "";
     isbn.value = "";
 });
-
-
-
-
-
-
-
-
-
-/*
-"rules": {
-    ".read": "now < 1608933600000",  // 2020-12-26
-    ".write": "now < 1608933600000",  // 2020-12-26
-  }
-*/
-// {"author":"some author","title":"some book","isbn":"some isbn"}

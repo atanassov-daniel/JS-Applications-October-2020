ac6805246c08980dc034c7c4c1cb3ab2ecf59851
atanassov-daniel <59175862+atanassov-daniel@users.noreply.github.com>
2020-12-01 23:26:13 +0200

    demo for a simple conception for templating

---
+++ 5Templating/Articles/articles.js
@@ -0,0 +1,23 @@
+export default [{
+    title: "First Article",
+    body: "1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ab?",
+    author: "Ivan Petrov"
+}, {
+    title: "Second Article",
+    body: "2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ab?",
+    author: "Stamat Goshev",
+    sponsored: true
+}, {
+    title: "Third Article",
+    body: "3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ab?",
+    author: "Stamat Goshev"
+}, {
+    title: "Fourth Article",
+    body: "4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ab?",
+    author: "Stamat Goshev",
+    sponsored: true
+}, {
+    title: "Fifth Article",
+    body: "5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ab?",
+    author: "Stamat Goshev"
+}];

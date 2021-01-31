646d0083b359bbf0ceb94d249cf91ea13c23a5a7
atanassov-daniel <59175862+atanassov-daniel@users.noreply.github.com>
2021-01-31 21:59:22 +0200

    validate if the user is authorized to see the page and respond accordingly

--- 7Workshop-SPA/movies(PushBasedRouting)/scripts/router.js
+++ 7Workshop-SPA/movies(PushBasedRouting)/scripts/router.js
@@ -19,6 +19,49 @@ const routes = {
     // '/addMovie/': 'add-movie-template',
     '/details': 'movie-details-template',
     '/edit': 'edit-movie-template',
+    '/forbidden': 'forbidden-template'
+};
+
+const validateAuthPages = async (path) => { // pass the key only if the page is '/edit/'
+    const nonAuthPages = ['/login', '/login/', '/register', '/register/'];
+    const authPages = ['/addMovie', '/addMovie/', '/logout'];
+
+    const isAuthPage = path.startsWith('/details/') || path.startsWith('/edit/') ||
+        authPages.includes(path);
+
+    let {
+        isAuthenticated,
+        email
+    } = authService.getAuthData();
+
+    if (isAuthPage) {
+        // if the page requires Auth, but the user isn't authorised, 'return false'
+        if (isAuthenticated === false) {
+            return false;
+        } else {
+            // if the user tries to edit an entry he isn't the creator of, 'return false'
+            if (path.startsWith('/edit')) {
+                let key = path.replace('/edit/', '');
+
+                let movieDetails = await movieService.getOne(key);
+
+                if (movieDetails._creator !== email) {
+                    return false; // if the user shouldn't be able to edit the entry, 'return false'
+                } else {
+                    return true; // if he is the creator, all is well, so 'return true'
+                }
+            } else {
+                return true; // if the person is Auth to view the page, 'return true'
+            }
+        }
+    } else if (nonAuthPages.includes(path)) { // if the page is a non-authorized one
+        // if the page is for non-Auth users, but the user is authorised, 'return false', for ex. for the login and register pages if the user has already logged in
+        if (isAuthenticated === true) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 };
 
 const router = async (url) => { // loads the content corresponding to the current page
@@ -33,6 +76,11 @@ const router = async (url) => { // loads the content corresponding to the curren
 
     let app = document.getElementById('app');
     let templateData = authService.getAuthData();
+
+    let isNotForbidden = await validateAuthPages(path);
+    if (isNotForbidden === false) {
+        path = '/forbidden';
+    }
 
     if (path === '/logout') { // log the user out and navigate to the home page
         authService.logout();
